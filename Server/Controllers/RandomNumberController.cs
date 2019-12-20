61115cc78885a9962cdc966dc93b4203c936f50d
Suzuki Taro <t-suzuki@indigo.co.jp>
2019-12-20 18:22:58 +0900

    add Random Number Generator
    path: GET /api/randomnumber/generate

---
+++ Server/Controllers/RandomNumberController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class RandomNumberController : Controller
+    {
+        [Authorize]
+        [HttpGet("Generate")]
+        public ActionResult<Int32> Generate()
+        {
+            return new Random().Next(0, 1000);
+        }
+    }
+}
