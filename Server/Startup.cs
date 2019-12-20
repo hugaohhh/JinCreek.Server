3a9196cf0ab089f2ef24f9e543c75f1b733d0138
Suzuki Taro <t-suzuki@indigo.co.jp>
2019-12-20 17:52:09 +0900

    disable HttpsRedirection (for debug!)

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -89,7 +89,7 @@ namespace Server
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
