7d9cf7d822170d59c976867bfca705332bcbc720
Suzuki Taro <t-suzuki@indigo.co.jp>
2019-12-25 14:28:12 +0900

    add Organization model
    se https://docs.microsoft.com/ja-jp/aspnet/core/tutorials/first-web-api?view=aspnetcore-3.1&tabs=visual-studio#add-a-model-class

--- Admin/Data/ApplicationDbContext.cs
+++ Admin/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Admin.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,7 @@ namespace Admin.Data
             : base(options)
         {
         }
+
+        public DbSet<Organization> Organizations { get; set; }
     }
 }
