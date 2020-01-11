cc3141ca354ea7aadbfce1f2a4a1aa3a8f9a32cf
Ryuji GUSHIKEN <gushiken@indigo.co.jp>
2020-01-11 10:14:15 +0900

    error responseをstatic readonlyで定義

--- Interface/ErrorResponse.cs
+++ Interface/ErrorResponse.cs
@@ -4,6 +4,9 @@ namespace JinCreek.Server.Interfaces
 {
     public class ErrorResponse
     {
+        public static readonly ErrorResponse NotMatchSimDevice = new ErrorResponse { ErrorCode = "1001", ErrorMessage = "Not Match SimDevice Info" };
+        public static readonly ErrorResponse NotMatchMultiFactor = new ErrorResponse { ErrorCode = "1002", ErrorMessage = "Not Match MultiFactor Info" };
+
         [Required]
         public string ErrorCode { get; set; }
 
