fcb11783f9eb4b38148a6e40632b1db1dc57501f
Ryuji GUSHIKEN <gushiken@indigo.co.jp>
2020-01-16 14:03:24 +0900

    サンプル実装

--- AuthTests/Controllers/SimDeviceAuthenticationController.cs
+++ AuthTests/Controllers/SimDeviceAuthenticationController.cs
@@ -1,60 +1,136 @@
-﻿using JinCreek.Server.Interfaces;
+﻿using JinCreek.Server.Auth;
+using JinCreek.Server.Common.Models;
+using JinCreek.Server.Interfaces;
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using JinCreek.Server.Common.Repositories;
-using Microsoft.AspNetCore.Mvc;
+using System.Net;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace JinCreek.Server.AuthTests.Controllers
 {
-    public class Sample1
+    [Collection("Sequential")]
+    public class ValidateTests : WebApplicationBase
     {
-        [Fact]
-        public void Case1()
+        public ValidateTests(WebApplicationFactory<Startup> factory, ITestOutputHelper testOutputHelper) : base(factory, testOutputHelper)
         {
-            Assert.Equal("1", "1");
-        }
-    }
-
-    class TestDataClass : IEnumerable<object[]>
-    {
-        List<object[]> _testData = new List<object[]>();
-
-        public TestDataClass()
-        {
-            //_testData.Add(new object[] { new SimDeviceAuthenticationRequest() { DeviceImei = "", SimIccId = "1" }, 200, new ValidationProblemDetails(){Status = 200} });
-            //_testData.Add(new object[] { new SimDeviceAuthenticationRequest() { DeviceImei = "1", SimIccId = "2" }, 200, new ErrorResponse(){ErrorCode = "1111", ErrorMessage = "aaaa"} });
-            //_testData.Add(new object[] { new SimDeviceAuthenticationRequest() { DeviceImei = "", SimIccId = "3" }, 400, new SimDeviceAuthenticationResponse(){AuthId = "hoge"} });
-            _testData.Add(new object[] { new SimDeviceAuthenticationRequest() { DeviceImei = "", SimIccId = "1" }, 200});
-            _testData.Add(new object[] { new SimDeviceAuthenticationRequest() { DeviceImei = "1", SimIccId = "2" }, 200});
-            _testData.Add(new object[] { new SimDeviceAuthenticationRequest() { DeviceImei = "", SimIccId = "3" }, 400});
         }
 
-        public IEnumerator<object[]> GetEnumerator() => _testData.GetEnumerator();
-
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-    }
-
-    public class Sample2
-    {
-        private readonly ITestOutputHelper _testOutputHelper;
-        private readonly AuthenticationRepository _repository;
-
-        public Sample2(ITestOutputHelper testOutputHelper, AuthenticationRepository authenticationRepository)
+        class TestDataClass : IEnumerable<object[]>
         {
-            _testOutputHelper = testOutputHelper;
-            _repository = authenticationRepository;
+            List<object[]> _testData = new List<object[]>();
+
+            public TestDataClass()
+            {
+                //_testData.Add(new object[] { new SimDeviceAuthenticationRequest() { DeviceImei = "", SimIccId = "1" }, 200, new ValidationProblemDetails(){Status = 200} });
+                //_testData.Add(new object[] { new SimDeviceAuthenticationRequest() { DeviceImei = "1", SimIccId = "2" }, 200, new ErrorResponse(){ErrorCode = "1111", ErrorMessage = "aaaa"} });
+                //_testData.Add(new object[] { new SimDeviceAuthenticationRequest() { DeviceImei = "", SimIccId = "3" }, 400, new SimDeviceAuthenticationResponse(){AuthId = "hoge"} });
+                _testData.Add(new object[] { new SimDeviceAuthenticationRequest() { DeviceImei = "", SimIccId = "1" }, HttpStatusCode.BadRequest });
+                _testData.Add(new object[] { new SimDeviceAuthenticationRequest() { DeviceImei = "1", SimIccId = "2" }, HttpStatusCode.BadRequest });
+                _testData.Add(new object[] { new SimDeviceAuthenticationRequest() { DeviceImei = "", SimIccId = "3" }, HttpStatusCode.BadRequest });
+            }
+
+            public IEnumerator<object[]> GetEnumerator() => _testData.GetEnumerator();
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
         }
 
-        [Theory(DisplayName = "サンプル")]
+
+        [Theory(DisplayName = "バリデーションテスト")]
         [ClassData(typeof(TestDataClass))]
-        public void Case10(SimDeviceAuthenticationRequest request, int statusCode)
+        public void Validation(SimDeviceAuthenticationRequest request, HttpStatusCode statusCode)
         {
-            _testOutputHelper.WriteLine(message: $">{request.DeviceImei}, {request.SimIccId}");
-            _testOutputHelper.WriteLine(message: $">{statusCode}");
-            //_testOutputHelper.WriteLine(message: $">{response}");
-            Assert.Equal("1", "1");
+            TestOutputHelper.WriteLine(message: $">{request.DeviceImei}, {request.SimIccId}");
+            TestOutputHelper.WriteLine(message: $">{statusCode}");
+
+
+            var acualRessult = PostSimDeviceAuthentication(request);
+
+            Assert.Equal(statusCode, acualRessult.StatusCode);
+
+            TestOutputHelper.WriteLine($"{acualRessult.StatusCode.ToString()}, {acualRessult.Content.ReadAsStringAsync().Result}, {acualRessult.Headers}");
+        }
+    }
+
+
+    [Collection("Sequential")]
+    public class DbTests : WebApplicationBase
+    {
+        public DbTests(WebApplicationFactory<Startup> factory, ITestOutputHelper testOutputHelper) : base(factory, testOutputHelper)
+        {
+        }
+
+        [Fact(DisplayName = "DBテストCaseXX")]
+        public void DbTestCaseXx()
+        {
+            var org = new Organization()
+            {
+                Code = "1234",
+                Name = "a",
+                Address = "b",
+                DelegatePhone = "0",
+                AdminPhone = "c",
+                AdminMail = "d",
+                Url = "url",
+                StartDay = DateTime.Now,
+                EndDay = DateTime.Now,
+                IsValid = true
+            };
+            UserRepository.Create(org);
+
+
+
+            var request = new SimDeviceAuthenticationRequest()
+            {
+                DeviceImei = "1",
+                SimIccId = "2",
+                SimImsi = "3",
+                SimMsisdn = "4"
+            };
+
+            var acualRessult = PostSimDeviceAuthentication(request);
+            var acualOrganization = UserRepository.GetOrganization(org.Id);
+
+
+            Assert.Equal(HttpStatusCode.Unauthorized, acualRessult.StatusCode);
+            Assert.Equal(org.Id, acualOrganization.Id);
+        }
+
+        [Fact(DisplayName = "DBテストCaseYy")]
+        public void DbTestCaseYy()
+        {
+            var org = new Organization()
+            {
+                Code = "1234",
+                Name = "a",
+                Address = "b",
+                DelegatePhone = "0",
+                AdminPhone = "c",
+                AdminMail = "d",
+                Url = "url",
+                StartDay = DateTime.Now,
+                EndDay = DateTime.Now,
+                IsValid = true
+            };
+            UserRepository.Create(org);
+
+
+
+            var request = new SimDeviceAuthenticationRequest()
+            {
+                DeviceImei = "1",
+                SimIccId = "2",
+                SimImsi = "3",
+                SimMsisdn = "4"
+            };
+
+            var acualRessult = PostSimDeviceAuthentication(request);
+            var acualOrganization = UserRepository.GetOrganization(org.Id);
+
+
+            Assert.Equal(HttpStatusCode.Unauthorized, acualRessult.StatusCode);
+            Assert.Equal(org.Id, acualOrganization.Id);
         }
     }
 }
---
+++ AuthTests/WebApplicationBase.cs
@@ -0,0 +1,96 @@
+﻿using JinCreek.Server.Common.Repositories;
+using JinCreek.Server.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Mime;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace JinCreek.Server.AuthTests
+{
+    public class WebApplicationBase : IClassFixture<WebApplicationFactory<Auth.Startup>>, IDisposable
+    {
+        protected readonly ITestOutputHelper TestOutputHelper;
+
+        protected readonly HttpClient Client;
+
+        protected readonly MainDbContext MainDbContext;
+        protected readonly RadiusDbContext RadiusDbContext;
+
+        protected readonly SimDeviceRepository SimDeviceRepository;
+        protected readonly UserRepository UserRepository;
+        protected readonly AuthenticationRepository AuthenticationRepository;
+
+        protected WebApplicationBase(WebApplicationFactory<Auth.Startup> factory, ITestOutputHelper testOutputHelper)
+        {
+            TestOutputHelper = testOutputHelper;
+            TestOutputHelper.WriteLine("Construct");
+
+            Client = factory.CreateClient();
+            Client.DefaultRequestHeaders.Accept.Clear();
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(MediaTypeNames.Application.Json));
+
+            var scope = factory.Services.GetService<IServiceScopeFactory>().CreateScope();
+            MainDbContext = scope.ServiceProvider.GetService<MainDbContext>();
+            RadiusDbContext = scope.ServiceProvider.GetService<RadiusDbContext>();
+
+            SimDeviceRepository = scope.ServiceProvider.GetService<SimDeviceRepository>();
+            UserRepository = scope.ServiceProvider.GetService<UserRepository>();
+            AuthenticationRepository = scope.ServiceProvider.GetService<AuthenticationRepository>();
+
+
+            TestOutputHelper.WriteLine("Database Init");
+            MainDbContext.Database.EnsureDeleted();
+            MainDbContext.Database.EnsureCreated();
+            MainDbContext.Database.Migrate();
+        }
+
+
+        public void Dispose()
+        {
+            TestOutputHelper.WriteLine("Dispose");
+            MainDbContext.Database.EnsureDeleted();
+        }
+
+
+        protected HttpResponseMessage PostSimDeviceAuthentication(SimDeviceAuthenticationRequest simDeviceAuthenticationRequest)
+        {
+            var url = "api/sim_device/authentication";
+            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, url)
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(simDeviceAuthenticationRequest), Encoding.UTF8, MediaTypeNames.Application.Json)
+            };
+            return Post(httpRequestMessage);
+        }
+
+        protected HttpResponseMessage PostMultiFactorAuthentication(MultiFactorAuthenticationRequest multiFactorAuthenticationRequest)
+        {
+            var url = "api/multi_factor/authentication";
+            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, url)
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(multiFactorAuthenticationRequest), Encoding.UTF8, MediaTypeNames.Application.Json)
+            };
+            return Post(httpRequestMessage);
+        }
+
+        protected HttpResponseMessage PostDeauthentication(DeauthenticationRequest deauthenticationRequest)
+        {
+            var url = "api/deauthentication";
+            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, url)
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(deauthenticationRequest), Encoding.UTF8, MediaTypeNames.Application.Json)
+            };
+            return Post(httpRequestMessage);
+        }
+
+        private HttpResponseMessage Post(HttpRequestMessage httpRequestMessage)
+        {
+            return Client.SendAsync(httpRequestMessage).Result;
+        }
+    }
+}
